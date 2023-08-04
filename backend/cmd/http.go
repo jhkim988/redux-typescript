2ab606ef90edb0fa29e3cba6e7f9c345d9c171d3
jinhankim988 <jinhankim988@douzone.com>
2023-08-04 11:20:21 +0900

    로그 포맷 수정

--- backend/cmd/http.go
+++ backend/cmd/http.go
@@ -14,7 +14,10 @@ func main() {
 	e.PUT("/api/todo/:id", EditTodo)
 	e.DELETE("/api/todo/:id", RemoveTodo)
 
-	e.Use(middleware.Logger())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format:           "method=${method}, uri=${uri}, status=${status}, time=${time_custom}\n",
+		CustomTimeFormat: "2006-01-02 15:04:05.00000",
+	}))
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{"*"},
